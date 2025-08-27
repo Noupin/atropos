26f722dc8e2422485ff90b9c9f2a09da86bca218
Noah Perkins <naperkins7@gmail.com>
2025-08-27 09:27:47 -0400

    more links

--- server/pipeline.py
+++ server/pipeline.py
@@ -34,10 +34,10 @@ from steps.candidates import ClipCandidate
 if __name__ == "__main__":
     overall_start = time.perf_counter()
 
-    #yt_url = "https://www.youtube.com/watch?v=GDbDRWzFfds" #KFAF 1
-    # yt_url = "https://www.youtube.com/watch?v=zZYxqZFThls" 
-    # yt_url = "https://www.youtube.com/watch?v=K9aFbYd6AUI" #Superman
-    yt_url = "https://www.youtube.com/watch?v=os2AyD_4RjM" #Dark phoenix
+    # yt_url = "https://www.youtube.com/watch?v=GDbDRWzFfds" #KFAF 1
+    # yt_url = "https://www.youtube.com/watch?v=zZYxqZFThls" #KFAF 2
+    yt_url = "https://www.youtube.com/watch?v=K9aFbYd6AUI" #Superman
+    # yt_url = "https://www.youtube.com/watch?v=os2AyD_4RjM" #Dark phoenix
     # yt_url = input("Enter YouTube video URL: ")
 
     CLIP_TYPE = "funny"  # change to 'inspiring' or 'educational'
