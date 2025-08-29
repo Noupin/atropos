00d43eee18d4a0bebdc3e79d796229d29fbdd51b
Noupin <naperkins7@gmail.com>
2025-08-29 13:04:20 -0400

    larger text

--- server/steps/config.py
+++ server/steps/config.py
@@ -1,6 +1,6 @@
 """Configuration constants for step modules."""
 
 # Default baseline caption font scale for rendered videos
-CAPTION_FONT_SCALE = 1.0
+CAPTION_FONT_SCALE = 2.0
 
 __all__ = ["CAPTION_FONT_SCALE"]
